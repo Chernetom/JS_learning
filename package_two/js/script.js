380f5774ea2ec7cc41451f10b269d06dae5d5f18
Chernetom <b3rberserker@yandex.ru>
2022-09-25 16:15:53 +0300

    'les_8'

--- package_two/js/script.js
+++ package_two/js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 window.addEventListener('DOMContentLoaded', () => {
+    //switch tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabHeader = document.querySelector('.tabheader__items'),
           tabContent = document.querySelectorAll('.tabcontent');
@@ -29,4 +30,55 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+    //timer
+    const deadline = '2022-09-31';
+    function getDataTimer(endtime){
+        const timer = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(timer / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((timer / (1000 * 60 * 60) % 24)),
+              minutes = Math.floor((timer / (1000 * 60) % 60)),
+              seconds = Math.floor((timer / 1000) % 60);
+        return {
+            'total': timer,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getZero(num){
+        if(num > 0 && num < 10){
+            return `0${num}`;
+        }else {
+            return num;
+        }
+    }
+
+    function setDataTimer(selector, endtime){
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateDataTimer, 1000);
+        
+        updateDataTimer();
+
+        function updateDataTimer(){
+            const timer = getDataTimer(endtime);
+
+            days.innerHTML = getZero(timer.days);
+            hours.innerHTML = getZero(timer.hours);
+            minutes.innerHTML = getZero(timer.minutes);
+            seconds.innerHTML = getZero(timer.seconds);
+
+            if (timer.total <= 0){
+                clearInterval( timeInterval);
+            }
+
+        }
+    }
+
+    setDataTimer('.timer', deadline);
 });
